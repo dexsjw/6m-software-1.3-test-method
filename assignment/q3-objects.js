b5253b6427c5d0f770e4a8246813c46fb8452189
dextersjw <dextersjw@gmail.com>
2024-06-06 17:25:34 +0800

    completed 1.3 assignment q3

--- assignment/q3-objects.js
+++ assignment/q3-objects.js
@@ -25,6 +25,14 @@ function convert(keyArr, valueArr){
         - Step 3: Within the for-loop, add the key and value to the local scoped object.
         - Step 4: Write a return statement to return the object literal after the for-loop code block.
     */
+    const convertedObj = {}
+
+    for (let i = 0; i < keyArr.length; i++) {
+        // console.log(keyArr[i], valueArr[i]);
+        convertedObj[keyArr[i]] = valueArr[i];
+    }
+
+    return convertedObj;
 }
 
 function printByKey(key){
